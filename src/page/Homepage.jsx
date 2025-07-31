067c1721f1627954f0236b2d94f4ce5cc6042c4d
Biddikub <93991379+Taechapat47@users.noreply.github.com>
2025-08-01 00:03:24 +0700

    Update Homepage.jsx

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -459,7 +459,7 @@ export default function DcaPage() {
                     "4px 4px  8px rgba(0,0,0,0.3) , 2px 2px 4px rgba(0,0,0,0.3)",
                 }}
               >
-                เริ่มทำแนวประเมินได้เลย!
+                เริ่มทำแบบประเมินได้เลย!
               </Link>
             </div>
           </div>
